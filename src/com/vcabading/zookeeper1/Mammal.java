504c7428e38c49510846976182a23b97d4ebfa4f
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-01 12:42:13 -0800

    add mammal energyLevel

--- src/com/vcabading/zookeeper1/Mammal.java
+++ src/com/vcabading/zookeeper1/Mammal.java
@@ -1,5 +1,21 @@
 package com.vcabading.zookeeper1;
 
 public class Mammal {
+	private int energyLevel;
+	
+	public Mammal() {
+		
+	}
 
+	//	//// GETTERS & SETTERS /////////////////////////////////////
+	
+	public int getEnergyLevel() {
+		return energyLevel;
+	}
+
+	public void setEnergyLevel(int energyLevel) {
+		this.energyLevel = energyLevel;
+	}
+	
+	
 }
