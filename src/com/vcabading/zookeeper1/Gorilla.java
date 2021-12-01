c210ff821065b38a6eeb4d1b83daf022eca1a2f6
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-01 14:37:05 -0800

    add throwSomethin eatBananas climb to Gorrilla

--- src/com/vcabading/zookeeper1/Gorilla.java
+++ src/com/vcabading/zookeeper1/Gorilla.java
@@ -2,4 +2,19 @@ package com.vcabading.zookeeper1;
 
 public class Gorilla extends Mammal {
 
+	//	//// ACTIONS ////////////////////////////////////
+	
+	public void throwSomething() {
+		
+	}
+	
+	public void eatBananas() {
+		
+	}
+	
+	public void clinb() {
+		
+	}
+	
+	
 }
