5c116f9c28be937a8d211937344eacdd7bfddb4c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-01 19:49:08 -0800

    add comments in Gorilla

--- src/com/vcabading/zookeeper1/Gorilla.java
+++ src/com/vcabading/zookeeper1/Gorilla.java
@@ -13,34 +13,30 @@ public class Gorilla extends Mammal {
 		super();
 		this.animalType = "Gorilla";
 	}
-
 	
 	//	//// ACTIONS ////////////////////////////////////
 	
+	//	---- Throw Something ----------------------------
+	//			when the gorilla throws something, it loses 5 energy
 	public void throwSomething() {
 		System.out.printf(".:: %s has thrown something ::.\n", this.animalType);
 		this.energyLevel -= 5;
 	}
 	
+	//	---- Eat Bananas --------------------------------
+	//			when the Gorilla eats bananas, it gains 10 energy
 	public void eatBananas() {
 		System.out.printf(".:: %s is satisfied with eating a banana ::.\n", this.animalType);
 		this.energyLevel += 10;
 	}
 	
+	//	---- Climb --------------------------------------
+	//			when a Gorilla climbs a tree, it loses 10 energy
 	public void climb() {
 		System.out.printf(".:: %s has climbed a tree ::.\n", this.animalType);
 		this.energyLevel -= 10;
 	}
 	
-
 	//	//// GET AND SET ////////////////////////////////
-	
-	public void setAnimalType(String animalType) {
-		this.animalType = animalType;
-	}
-	
-	public String getAnimalType() {
-		return animalType;
-	}
 	
 }
